7dbebd1fdc479ee0155dd71ae4922abd933689d8
Yaoi987 <46786314+Yaoi987@users.noreply.github.com>
2019-02-05 10:25:35 -0500

    Updated the UI to contain only one date field, changed the font size, and made the button parse string to Date

--- src/amazcart2/ImageUI.java
+++ src/amazcart2/ImageUI.java
@@ -4,6 +4,8 @@ import javax.swing.*;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class ImageUI {
     JPanel populate() {
@@ -13,28 +15,41 @@ public class ImageUI {
         c.insets=new Insets(10,10,10,10);
         c.gridy=0;
         c.gridx=1;
-        p.add(new JLabel("From:"),c);
+        JLabel temp = new JLabel("End Date:");
+        temp.setFont(new Font("Big text",Font.PLAIN,50));
+        p.add(temp,c);
         c.gridx=2;
-        p.add(new JTextField(10),c);
-        c.gridx=4;
-        p.add(new JLabel("To: "),c);
-        c.gridx=5;
-        p.add(new JTextField(10),c);
+        JTextField tempField = new JTextField(7);
+        tempField.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {DisplayImages(p, tempField);}
+        });
+        tempField.setFont(new Font("Big text",Font.PLAIN,50));
+        p.add(tempField,c);
+        c.gridx=1;
         c.gridy=1;
-        c.gridx=3;
-        c.anchor=c.CENTER;
-        JButton temp = new JButton("Search");
-        temp.addActionListener(new ActionListener() {
+        c.fill=c.CENTER;
+        c.gridwidth=2;
+        JButton tempButton = new JButton("Search");
+        tempButton.setFont(new Font("Big text",Font.PLAIN,50));
+        tempButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                DisplayImages(p);
+                DisplayImages(p, tempField);
             }
         });
-        p.add(temp,c);
+        p.add(tempButton,c);
         return p;
     }
-    public void DisplayImages(JPanel p) {
-
+    public void DisplayImages(JPanel p, JTextField field) {
+        SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
+        try {
+            //formatter.setLenient(false);
+            Date EndDate = formatter.parse(field.getText());
+            System.out.println(EndDate);
+        } catch (Exception ex) {
+            System.err.println("Unformatted Date");
+        }
     }
 
     public static void main(String[] args) {
