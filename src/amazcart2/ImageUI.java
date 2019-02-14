e7c963b0f4cf5a474842b8d9f88aafda3c885a17
Trevor <lyfeslap@gmail.com>
2019-02-14 14:15:24 -0500

    Cleaned up code

--- src/amazcart2/ImageUI.java
+++ src/amazcart2/ImageUI.java
@@ -8,141 +8,154 @@ import java.text.SimpleDateFormat;
 import java.util.Date;
 
 public class ImageUI {
-    public ImageUI() {
+    private ImageUI() {
         JFrame frame = new JFrame();
         frame.setTitle("Sealed Fate");
-        frame.add(this.populate(frame));
+        frame.add(Populate(frame));
         frame.pack();
         frame.setLocationRelativeTo(null);
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setResizable(false);
         frame.setVisible(true);
     }
-    JPanel populate(JFrame frame) {
+
+    //Populate the frame
+    private JPanel Populate(JFrame frame) {
         JPanel panel = new JPanel(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
-        constraints.anchor=constraints.NORTH;
-        constraints.insets=new Insets(10,10,10,10);
-        constraints.gridy=0;
-        constraints.gridx=1;
+        constraints.anchor = GridBagConstraints.NORTH;
+        constraints.insets = new Insets(10, 10, 10, 10);
+        constraints.gridy = 0;
+        constraints.gridx = 1;
         JLabel tempLabel = new JLabel("End Date:");
-        tempLabel.setFont(new Font("Big text",Font.PLAIN,50));
-        panel.add(tempLabel,constraints);
-        constraints.gridx=2;
+        tempLabel.setFont(new Font("Big text", Font.PLAIN, 50));
+        panel.add(tempLabel, constraints);
+        constraints.gridx = 2;
         JTextField tempField = new JTextField(7);
         tempField.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {DisplayImages(frame,panel, tempField);}
+            public void actionPerformed(ActionEvent e) {
+                DisplayImages(frame, panel, tempField);
+            }
         });
-        tempField.setFont(new Font("Big text",Font.PLAIN,50));
-        panel.add(tempField,constraints);
-        constraints.gridx=1;
-        constraints.gridy=1;
-        constraints.fill=constraints.CENTER;
-        constraints.gridwidth=2;
+        tempField.setFont(new Font("Big text", Font.PLAIN, 50));
+        panel.add(tempField, constraints);
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        constraints.fill = GridBagConstraints.CENTER;
+        constraints.gridwidth = 2;
         JButton tempButton = new JButton("Search");
-        tempButton.setFont(new Font("Big text",Font.PLAIN,50));
+        tempButton.setFont(new Font("Big text", Font.PLAIN, 50));
         tempButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                DisplayImages(frame, panel,tempField);
+                DisplayImages(frame, panel, tempField);
             }
         });
-        panel.add(tempButton,constraints);
+        panel.add(tempButton, constraints);
         return panel;
     }
-    JPanel populatePic(JFrame frame,Date EndDate) {
+
+    //Populate with pictures
+    private JPanel PopulatePic(JFrame frame, Date EndDate) {
         JPanel panel = new JPanel(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
-        constraints.anchor=constraints.NORTH;
-        constraints.insets=new Insets(10,10,10,10);
-        constraints.gridy=0;
-        constraints.gridx=1;
+        constraints.anchor = GridBagConstraints.NORTH;
+        constraints.insets = new Insets(10, 10, 10, 10);
+        constraints.gridy = 0;
+        constraints.gridx = 1;
         JLabel tempLabel = new JLabel("End Date:");
-        tempLabel.setFont(new Font("Big text",Font.PLAIN,50));
-        panel.add(tempLabel,constraints);
-        constraints.gridx=2;
+        tempLabel.setFont(new Font("Big text", Font.PLAIN, 50));
+        panel.add(tempLabel, constraints);
+        constraints.gridx = 2;
         JTextField tempField = new JTextField(7);
         tempField.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {DisplayImages(frame,panel, tempField);}
+            public void actionPerformed(ActionEvent e) {
+                DisplayImages(frame, panel, tempField);
+            }
         });
-        tempField.setFont(new Font("Big text",Font.PLAIN,50));
-        panel.add(tempField,constraints);
-        constraints.gridx=1;
-        constraints.gridy=1;
-        constraints.fill=constraints.CENTER;
-        constraints.gridwidth=2;
+        tempField.setFont(new Font("Big text", Font.PLAIN, 50));
+        panel.add(tempField, constraints);
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        constraints.fill = GridBagConstraints.CENTER;
+        constraints.gridwidth = 2;
         JButton tempButton = new JButton("Search");
-        tempButton.setFont(new Font("Big text",Font.PLAIN,50));
+        tempButton.setFont(new Font("Big text", Font.PLAIN, 50));
         tempButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                DisplayImages(frame, panel,tempField);
+                DisplayImages(frame, panel, tempField);
             }
         });
-        panel.add(tempButton,constraints);
-        constraints.gridx=1;
-        constraints.gridy=2;
-        constraints.fill=constraints.CENTER;
-        constraints.gridwidth=2;
+        panel.add(tempButton, constraints);
+        constraints.gridx = 1;
+        constraints.gridy = 2;
+        constraints.fill = GridBagConstraints.CENTER;
+        constraints.gridwidth = 2;
         JLabel dateLabel = new JLabel(EndDate.toString(), SwingConstants.CENTER);
-        panel.add(dateLabel,constraints);
+        panel.add(dateLabel, constraints);
         return panel;
     }
-    JPanel DisplayError(JFrame frame) {
+
+    //Displays user errors
+    private JPanel DisplayError(JFrame frame) {
         JPanel panel = new JPanel(new GridBagLayout());
         GridBagConstraints constraints = new GridBagConstraints();
-        constraints.anchor=constraints.NORTH;
-        constraints.insets=new Insets(10,10,10,10);
-        constraints.gridy=0;
-        constraints.gridx=1;
+        constraints.anchor = GridBagConstraints.NORTH;
+        constraints.insets = new Insets(10, 10, 10, 10);
+        constraints.gridy = 0;
+        constraints.gridx = 1;
         JLabel tempLabel = new JLabel("End Date:");
-        tempLabel.setFont(new Font("Big text",Font.PLAIN,50));
-        panel.add(tempLabel,constraints);
-        constraints.gridx=2;
+        tempLabel.setFont(new Font("Big text", Font.PLAIN, 50));
+        panel.add(tempLabel, constraints);
+        constraints.gridx = 2;
         JTextField tempField = new JTextField(7);
         tempField.addActionListener(new ActionListener() {
             @Override
-            public void actionPerformed(ActionEvent e) {DisplayImages(frame,panel, tempField);}
+            public void actionPerformed(ActionEvent e) {
+                DisplayImages(frame, panel, tempField);
+            }
         });
-        tempField.setFont(new Font("Big text",Font.PLAIN,50));
-        panel.add(tempField,constraints);
-        constraints.gridx=1;
-        constraints.gridy=1;
-        constraints.fill=constraints.CENTER;
-        constraints.gridwidth=2;
+        tempField.setFont(new Font("Big text", Font.PLAIN, 50));
+        panel.add(tempField, constraints);
+        constraints.gridx = 1;
+        constraints.gridy = 1;
+        constraints.fill = GridBagConstraints.CENTER;
+        constraints.gridwidth = 2;
         JButton tempButton = new JButton("Search");
-        tempButton.setFont(new Font("Big text",Font.PLAIN,50));
+        tempButton.setFont(new Font("Big text", Font.PLAIN, 50));
         tempButton.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
-                DisplayImages(frame, panel,tempField);
+                DisplayImages(frame, panel, tempField);
             }
         });
-        panel.add(tempButton,constraints);
-        constraints.gridx=1;
-        constraints.gridy=2;
-        constraints.fill=constraints.CENTER;
-        constraints.gridwidth=2;
+        panel.add(tempButton, constraints);
+        constraints.gridx = 1;
+        constraints.gridy = 2;
+        constraints.fill = GridBagConstraints.CENTER;
+        constraints.gridwidth = 2;
         JLabel incorrectFormatLabel = new JLabel("Incorrect Date Format...Use MM/DD/YYYY", SwingConstants.CENTER);
-        incorrectFormatLabel.setFont(new Font("Big text",Font.PLAIN,30));
-        panel.add(incorrectFormatLabel,constraints);
+        incorrectFormatLabel.setFont(new Font("Big text", Font.PLAIN, 30));
+        panel.add(incorrectFormatLabel, constraints);
         return panel;
     }
-    public void DisplayImages(JFrame frame, JPanel p, JTextField field) {
+
+    private void DisplayImages(JFrame frame, JPanel p, JTextField field) {
         SimpleDateFormat formatter = new SimpleDateFormat("MM/dd/yyyy");
         try {
             Date EndDate = formatter.parse(field.getText());
             frame.remove(p);
-            frame.add(this.populatePic(frame, EndDate));
+            frame.add(PopulatePic(frame, EndDate));
             frame.revalidate();
             frame.pack();
             frame.setVisible(true);
             frame.repaint();
         } catch (Exception ex) {
             frame.remove(p);
-            frame.add(this.DisplayError(frame));
+            frame.add(DisplayError(frame));
             frame.revalidate();
             frame.pack();
             frame.setVisible(true);
@@ -150,5 +163,7 @@ public class ImageUI {
         }
     }
 
-    public static void main(String[] args) { ImageUI UI = new ImageUI(); }
+    public static void main(String[] args) {
+        ImageUI UI = new ImageUI();
+    }
 }
