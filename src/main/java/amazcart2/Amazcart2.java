5677870b25eed66edafb7b1974017c3b273f2e39
TforTrevor <36804391+TforTrevor@users.noreply.github.com>
2019-02-17 17:28:55 -0500

    Import classes

---
+++ src/main/java/amazcart2/Amazcart2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package amazcart2;
+
+/**
+ *
+ * @author Jpc
+ */
+public class Amazcart2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        FileReader name= new FileReader();
+        name.readFile();
+        
+    }
+
+}
