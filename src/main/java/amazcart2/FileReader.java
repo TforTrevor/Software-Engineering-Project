898c119ded0d86758078195848a216b1fb98701b
rewind987 <godboutb@bellsouth.net>
2019-03-27 13:33:16 -0400

    Clean up FileReader

--- src/main/java/amazcart2/FileReader.java
+++ src/main/java/amazcart2/FileReader.java
@@ -1,26 +1,15 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package amazcart2;
 
 import java.io.File;
-import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
 import java.nio.file.attribute.BasicFileAttributes;
-import java.nio.file.attribute.FileTime;
-import java.text.DateFormat;
 import java.text.Format;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 
-/**
- *
- */
 public class FileReader {
     private String date;
     //"12/13/2018";
@@ -44,82 +33,46 @@ public class FileReader {
     }
 
     public void readFile() {
-        //System.out.println(System.getProperty("../ ")+"\\..");
         String fileLocation = "";
 
         while (new File(fileLocation).exists()) {
             fileLocation = fileLocation + "../";
         }
         File folder = new File(fileSearchLocation);
-        // folder=new File(folder.getParent());
-        //folder=new File(folder.getParent());
-        // folder=new File(folder.getParent());
-        //System.out.println(folder.getAbsolutePath());
-        //for(File file:folder.listFiles())
-        //{
-        //   System.out.println(file.getName());
-        //}
-        //searchDirForFile("C:\\");
+
         filesInFolder(folder);
     }
 
     public void filesInFolder(final File folder) {
-        //System.out.println(folder.getName());
         for (final File file : folder.listFiles()) {
             try {
                 if (file.isDirectory()) {
-                    // System.out.println(file.getAbsolutePath());
-                    //  System.out.println(file);
-
                     filesInFolder(file);
 
                 } else if (file.getName().toLowerCase().contains(".jpg") || file.getName().toLowerCase().contains(".png")) {
                     Path file2 = Paths.get(file.getAbsolutePath());
                     BasicFileAttributes attr = Files.readAttributes(file2, BasicFileAttributes.class);
-            /*    if(filter(attr.creationTime().toString(),convertTime(date))==true) {
 
-                    System.out.println(file.getName());
-
-                   System.out.println(convertTime(file.lastModified()));
-                }
-*/
                     String endDate3 = convertTime(endDate);
                     String startDate3 = convertTime(startDate);
                     Integer endYear = Integer.parseInt(endDate3.substring(0, 4));
                     Integer startYear = Integer.parseInt(startDate3.substring(0, 4));
                     if (endYear.equals(startYear + 1)) {
-
                         String endDate1 = "12/31/" + startYear;
-                        // System.out.println(endDate1);
                         String startDate2 = "01/01/" + endYear;
-                        // System.out.println(startDate2);
                         if (filter2(attr.creationTime().toString(), convertTime(startDate), convertTime(endDate1)) == true) {
 
-                            filesArr.add(file.getAbsolutePath()+file.getName());
-//                            System.out.println(file.getName());
-//                            System.out.println(convertTime(attr.creationTime().toMillis()));
+                            filesArr.add(file.getAbsolutePath() + file.getName());
                         }
                         if (filter2(attr.creationTime().toString(), convertTime(startDate2), convertTime(endDate)) == true) {
-                            filesArr.add(file.getAbsolutePath()+file.getName());
-//                            System.out.println(file.getName());
-//                            System.out.println(convertTime(attr.creationTime().toMillis()));
+                            filesArr.add(file.getAbsolutePath() + file.getName());
                         }
 
                     } else {
                         if (filter2(attr.creationTime().toString(), convertTime(startDate), convertTime(endDate)) == true) {
-                            filesArr.add(file.getAbsolutePath()+file.getName());
-//                            System.out.println(file.getName());
-//                            System.out.println(convertTime(attr.creationTime().toMillis()));
+                            filesArr.add(file.getAbsolutePath() + file.getName());
                         }
                     }
-           /* FileTime fileTime;
-try {
-    fileTime = Files.getLastModifiedTime(Paths.get(file.getAbsolutePath()));
-    printFileTime(fileTime);
-} catch (IOException e) {
-    System.err.println("Cannot get the last modified time - " + e);
-}
-            */
                 }
 
             } catch (Exception ignore) {
@@ -131,10 +84,6 @@ try {
     private boolean filter(String lastModified, String inputDate) {
         if (Integer.parseInt(lastModified.substring(0, 4)) == Integer.parseInt(inputDate.substring(0, 4))) {
             if (Integer.parseInt(lastModified.substring(5, 7)) >= Integer.parseInt(inputDate.substring(5, 7)) && Integer.parseInt(lastModified.substring(5, 7)) <= Integer.parseInt(inputDate.substring(5, 7)) + 2) {
-
-                // System.out.println(lastModified);
-                //System.out.println(lastModified.substring(8, 10));
-
                 if (Integer.parseInt(lastModified.substring(8, 10)) >= Integer.parseInt(inputDate.substring(8, 10))) {
                     return true;
                 }
@@ -146,9 +95,11 @@ try {
         }
         return false;
     }
+
     public ArrayList<String> getArr() {
         return filesArr;
     }
+
     private boolean filter2(String modifiedDate, String startDate, String endDate) {
 
         Integer modYear = Integer.parseInt(modifiedDate.substring(0, 4));
@@ -157,22 +108,6 @@ try {
         Integer modMonth = Integer.parseInt(modifiedDate.substring(5, 7));
         Integer startMonth = Integer.parseInt(startDate.substring(5, 7));
         Integer endMonth = Integer.parseInt(endDate.substring(5, 7));
-       /* if(endYear.equals(startYear+1))
-        {
-//2016 09 29 19:32:39 found fix algo
-            if(modYear.equals(startYear) || modYear.equals(endYear)) {
-                if (modMonth <= endMonth) {
-                    Integer modDay = Integer.parseInt(modifiedDate.substring(8, 10));
-                    Integer startDay = Integer.parseInt(startDate.substring(8, 10));
-                    Integer endDay = Integer.parseInt(endDate.substring(8, 10));
-                    if (modDay >= startDay && modDay <= endDay) {
-                        return true;
-                    }
-
-                }
-            }
-        }
-        */
         if (modYear.equals(startYear) || modYear.equals(endYear)) {
             if (modMonth > startMonth && modMonth < endMonth) {
                 return true;
@@ -185,22 +120,6 @@ try {
                 }
             }
         }
-     /*  else if(endYear.equals(startYear+1))
-        {
-           if(modYear<)
-            Integer modMonth=Integer.parseInt(modifiedDate.substring(5,7));
-            Integer startMonth=Integer.parseInt(startDate.substring(5,7));
-            Integer endMonth=Integer.parseInt(endDate.substring(5,7));
-            if(modMonth <= endMonth) {
-                Integer modDay = Integer.parseInt(modifiedDate.substring(8, 10));
-                Integer startDay = Integer.parseInt(startDate.substring(8, 10));
-                Integer endDay = Integer.parseInt(endDate.substring(8, 10));
-                if (modDay >= startDay && modDay <= endDay) {
-                    return true;
-                }
-            }
-            */
-
         return false;
     }
 
@@ -211,7 +130,6 @@ try {
     }
 
     public String convertTime(String time) {
-
         Date date = new Date(time);
         Format format = new SimpleDateFormat("yyyy MM dd HH:mm:ss");
         return format.format(date);
