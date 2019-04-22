9d656371f11390b25f438f5cba6ddba8281a4619
rewind987 <godboutb@bellsouth.net>
2019-04-22 16:08:59 -0400

    checks orders of dates

--- src/main/java/com/sep/ImageSearcher.java
+++ src/main/java/com/sep/ImageSearcher.java
@@ -70,6 +70,10 @@ public class ImageSearcher {
             return false;
         }
 
+        if(!startDate.getValue().isBefore(endDate.getValue())) {
+            return false;
+        }
+
         if (!searchThread.isAlive()) {
             searchThread = new Thread(this::SearchThread);
             searchThread.setDaemon(true);
